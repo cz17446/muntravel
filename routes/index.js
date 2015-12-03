262461142225af591d746e8ebf0e291c3cc98c4a
Chao <Chao@Chaos-MacBook-Pro.local>
2015-12-03 04:28:02 -0330

    deleted list working

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,7 @@ router.post('/geolocation', function(req, res) {
     latitude = req.body.latitude;
     longitude = req.body.longitude;
     console.log("location received by node: " + latitude + longitude);
+    res.sendStatus(200);
 })
 
 /* POST to Add Review Service */
@@ -44,12 +45,15 @@ router.post('/addreview', function(req, res) {
     var collection = db.get('userreview');
 
     // Submit to the DB
+    //TODO: city, and googlemap string
     collection.insert({
+        "timestamp" : new Date(),
+        "status" : "posted",
         "username" : username,
         "usertype": usertype,
         "reviewcomment" : reviewcomment,
         "replyusername": null,
-        "replyuserrole": null,
+        "replyusertype": null,
         "replycomment": null,
         "latitude": latitude,
         "longitude": longitude
@@ -63,4 +67,65 @@ router.post('/addreview', function(req, res) {
         }
     });
 });
+
+/* POST to update Review Service */
+router.post('/updatereview', function(req, res) {
+    console.log("Start to update to MongoDB: " + req);
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var reviewid = req.body.reviewid;
+    var username = req.body.username;
+    var usertype = req.body.usertype;
+    var reviewcomment= req.body.reviewcomment;
+
+    console.log("reviewid " + reviewid)
+
+    // Set our collection
+    var collection = db.get('userreview');
+
+    // Submit to the DB
+    collection.update({_id : reviewid}, 
+        {$set: {
+            "replyusername": username,
+            "replyusertype": usertype,
+            "replycomment": reviewcomment,
+            }
+        }, 
+        function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send("There was a problem adding the information to the database.");
+            }
+            else {
+                res.redirect("lasvegas");
+            }
+    });
+});
+
+/* POST to Delete Review Service */
+router.post('/deletereview', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userreview');
+    var reviewid = req.body.reviewid;
+    
+    // Submit to the DB
+    collection.update({_id : reviewid}, 
+        {$set: {
+            "status" : "deleted"
+            }
+        }, 
+        function (err, doc) {
+            if (err) {
+                // If it failed, return error
+                res.send("There was a problem adding the information to the database.");
+            }
+            else {
+                res.redirect("lasvegas");
+            }
+    });
+});
+
+
 module.exports = router;
--- routes/lasvegas.js
+++ routes/lasvegas.js
@@ -4,10 +4,15 @@ var router = express.Router();
 function lasvegas(req, res) {
     var db = req.db;
     var collection = db.get('userreview');
-    collection.find({},{},function(e,docs){
-        res.render('lasvegas', {
-            "reviewlist" : docs
-        });
+    collection.find({status: "posted"},{ sort : { timestamp : -1 } }, function(err, cursor){
+        console.log("search posted comments");
+      collection.find({status: "deleted"},{ sort : { timestamp : -1 } },function(e,docs){
+          console.log("search deleted comments");
+          res.render('lasvegas', {
+              "reviewlist" : cursor,
+              "deletedreviewlist" : docs
+          });
+      });
     });
 }
 
